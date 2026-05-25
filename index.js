6707b87257665fff16d9ef3af91b0be9f6190ed1
victoriawanko-create. <victoriawanko@gmail.com.>
2026-05-25 08:47:05 -0400

    update 5/25/26

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 // service_o2rb4xb
 // lpAmGWo6wLF2IDyPN
 
-function contact() {
+function contact(event) {
     event.preventDefault();
-    emailjs
-    .sendForm(
-       'service_o2rb4xb',
-       'template_mdiov1z',
-       event.target,
-       'user_lpAmGWo6wLF2IDyPN'
-    ).then(() => {
-        console.log('this worked')
-    })
+
+    // emailjs
+    // .sendForm(
+    //    'service_o2rb4xb',
+    //    'template_mdiov1z',
+    //    event.target,
+    //    'user_lpAmGWo6wLF2IDyPN'
+    // )
+    // .then(() => {
+    //     console.log('this worked')
+    // })
+    const loading = document.querySelector('modal))overlay--loading');
+    const success = document.querySelector('modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() =>{
+        console.log('it worked')
+    }, 500);
 
 }
